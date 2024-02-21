bb14778b7108ad9aeacc2cf2589d018bdf6414e5
Dieter <160256185+HD-Ruoff@users.noreply.github.com>
2024-02-21 18:20:42 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,53 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    Zahl += -1
-    I2C_LCD1602.ShowNumber(Zahl, 0, 0)
-})
-input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    if (Zahl < Zufall) {
-        basic.showIcon(IconNames.ArrowNorth)
-    } else if (Zahl > Zufall) {
-        basic.showIcon(IconNames.ArrowSouth)
-    } else {
-        basic.showIcon(IconNames.Happy)
-        I2C_LCD1602.ShowNumber(Zahl, 0, 0)
-        basic.setLedColor(0x00ff00)
-        I2C_LCD1602.clear()
+    if (ausgewählt == 0) {
+        if (Auswahl > 1) {
+            Auswahl += -1
+        }
+        basic.showNumber(Auswahl)
+        I2C_LCD1602.ShowNumber(Auswahl, 0, 0)
     }
 })
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    ausgewählt = 1
+    basic.setLedColor(0x00ff00)
+})
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    Zahl += 1
-    I2C_LCD1602.ShowNumber(Zahl, 0, 0)
+    if (ausgewählt == 0) {
+        Auswahl += 1
+    }
+    basic.showNumber(Auswahl)
+    I2C_LCD1602.ShowNumber(Auswahl, 0, 0)
+})
+function Intro () {
+    I2C_LCD1602.ShowString("Errate meine Zahl XX", 0, 0)
+    basic.pause(500)
+    basic.clearScreen()
+    I2C_LCD1602.clear()
+    basic.pause(200)
+}
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+    PIN0 = 1
+    basic.turnRgbLedOff()
+    if (ausgewählt == 1) {
+        basic.clearScreen()
+        I2C_LCD1602.clear()
+        Zahl = randint(0, Auswahl - 1)
+    }
 })
 let Zahl = 0
-let Zufall = 0
+let ausgewählt = 0
+let Auswahl = 0
+let PIN0 = 0
 I2C_LCD1602.LcdInit(39)
-Zufall = randint(1, 9)
-Zahl = 5
-I2C_LCD1602.ShowNumber(Zufall, 0, 1)
-basic.showNumber(Zufall)
-basic.pause(2000)
-basic.clearScreen()
 I2C_LCD1602.clear()
+PIN0 = 1
+Auswahl = 1
+basic.showNumber(1)
+I2C_LCD1602.ShowNumber(1, 0, 1)
+Intro()
+basic.forever(function () {
+    if (ausgewählt == 1 && PIN0 == 1) {
+        basic.showNumber(Zahl + 1)
+        I2C_LCD1602.ShowNumber(Zahl + 1, 0, 0)
+    }
+})
